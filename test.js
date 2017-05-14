6b2fdd3934aa1f64ada4c7541c1ab67bd52bff9e
Nils Knappmeier <github@knappi.org>
2017-05-14 22:05:49 +0200

    Initial checkin

---
+++ test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Dependencies
+ */
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
+
+/**
+ * Settings
+ */
+const PATH_HELPERS = path.resolve('./app/helpers/transform/');
+
+/**
+ * Register handlebars helpers
+ */
+fs.readdirSync(PATH_HELPERS)
+  .forEach(file => {
+    const ext = path.extname(file);
+    const name = path.basename(file, ext);
+    const helperPath = path.join(PATH_HELPERS, file);
+    const helper = require(helperPath);
+    handlebars.registerHelper(name, helper);
+  });
+
+var template = handlebars.compile('{{shout name}}')
+console.log(template({ name: 'Nils' }))
+
